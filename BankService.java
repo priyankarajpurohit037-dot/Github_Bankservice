5f0a56cb0ac08d7e7008d65a2bfae5013f88e3b7
priyankarajpurohit037-dot <priyankarajpurohit037@gmail.com>
2026-01-29 23:13:54 +0530

    Update BankService.java

--- BankService.java
+++ BankService.java
@@ -37,7 +37,7 @@ public class BankService {
         acc.deposit(700);
         System.out.println("Balance after deposit of 700: " + acc.getBalance());
 
-        acc.withdraw(300);
+        acc.withdraw(100);
         System.out.println("Balance after withdrawal of 100: " + acc.getBalance());
 
         System.out.println("Final Balance: " + acc.getBalance());
